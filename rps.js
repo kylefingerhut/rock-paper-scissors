18ec12d10efad7d13d6d42410074a92021ac5b09
kylefingerhut <fingerhutkyle@gmail.com>
2022-11-28 19:40:58 +0100

    randomly get one of three numbers

--- rps.js
+++ rps.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+// gets number 0, 1, or 2 randomly
+function getRandomInt() {
+    window.randomInt = Math.floor(Math.random() * 3);
+}
+
+getRandomInt();
+
+console.log(randomInt)
