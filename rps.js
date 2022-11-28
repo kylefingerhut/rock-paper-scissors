28b727378dc466a06be99cc71f3a99be99e29109
kylefingerhut <fingerhutkyle@gmail.com>
2022-11-28 20:06:25 +0100

    Make game work but not written optimally

--- rps.js
+++ rps.js
@@ -1,12 +1,41 @@
 // gets number 0, 1, or 2 randomly and assigns number one of three moves
 switch (Math.floor(Math.random() * 3)) {
     case 0:
-        move = "rock";
+        computerSelection = "rock";
         break;
     case 1:
-        move = "paper";
+        computerSelection = "paper";
         break;
     case 2:
-        move = "scissors";
+        computerSelection = "scissors";
         break;
+}
+
+let playerSelection = "scissors";
+
+if (computerSelection == "rock" && playerSelection == "scissors") {
+    console.log("Computer played rock. You lost. Rock beats scissors!");
+}
+
+if (computerSelection == "paper" && playerSelection == "rock") {
+    console.log("Computer played paper. You lost. Paper beats rock!");
+}
+
+if (computerSelection == "scissors" && playerSelection == "paper") {
+    console.log("Computer played scissors. You lost. Scissors beats paper!");
+}
+if (playerSelection == "rock" && computerSelection == "scissors") {
+    console.log("You played rock. You won. Rock beats scissors!");
+}
+
+if (playerSelection == "paper" && computerSelection == "rock") {
+    console.log("You played paper. You won. Paper beats rock!");
+}
+
+if (playerSelection == "scissors" && computerSelection == "paper") {
+    console.log("You played scissors. You won. Scissors beats paper!");
+}
+
+if (playerSelection == computerSelection) {
+    console.log("You both played " + playerSelection + ". No one wins.");
 }
