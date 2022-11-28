ff13a43eb77ac77df17a84354c8dda33bba2d5b4
kylefingerhut <fingerhutkyle@gmail.com>
2022-11-28 23:30:13 +0100

    Experiment

--- rps.js
+++ rps.js
@@ -1,5 +1,7 @@
 // gets random number (0, 1, or 2) and assigns the computer's move based on the number
-switch (Math.floor(Math.random() * 3)) {
+function getComputerChoice() {
+    let number = Math.floor(Math.random() * 3)
+    switch (number) {
     case 0:
         computerSelection = "rock";
         break;
@@ -10,25 +12,46 @@ switch (Math.floor(Math.random() * 3)) {
         computerSelection = "scissors";
         break;
 }
+return computerSelection;
+}
+
+getComputerChoice();
+
 
 // input the player's move
-let playerSelection = "scissors";
+let initialPlayerSelection = "scissors";
+let playerSelection = initialPlayerSelection.toLowerCase();
 
 // display game results
-if (computerSelection == "rock" && playerSelection == "scissors") {
-    console.log("Computer played rock. You lost. Rock beats scissors!");
-} else if (computerSelection == "paper" && playerSelection == "rock") {
-    console.log("Computer played paper. You lost. Paper beats rock!");
-} else if (computerSelection == "scissors" && playerSelection == "paper") {
-    console.log("Computer played scissors. You lost. Scissors beats paper!");
-} else if (playerSelection == "rock" && computerSelection == "scissors") {
-    console.log("You played rock. You won. Rock beats scissors!");
-} else if (playerSelection == "paper" && computerSelection == "rock") {
-    console.log("You played paper. You won. Paper beats rock!");
-} else if (playerSelection == "scissors" && computerSelection == "paper") {
-    console.log("You played scissors. You won. Scissors beats paper!");
-} else if (playerSelection == computerSelection) {
-    console.log("You both played " + playerSelection + ". No one wins.");
+function playRound(x, y) {
+    let result = " "
+
+if (x == "rock" && y == "scissors") {
+    result = "You lost. Computer played rock. Rock beats scissors!";
+} else if (x == "paper" && y == "rock") {
+    result = "You lost. Computer played paper. Paper beats rock!";
+} else if (x == "scissors" && y == "paper") {
+    result = "You lost. Computer played scissors. Scissors beats paper!";
+} else if (y == "rock" && x == "scissors") {
+    result = "You won. You played rock. Rock beats scissors!";
+} else if (y == "paper" && x == "rock") {
+    result = "You won. You played paper. Paper beats rock!";
+} else if (y == "scissors" && x == "paper") {
+    result = "You won. You played scissors. Scissors beats paper!";
+} else if (y == x) {
+    result = "You both played " + y + ". No one wins.";
 } else {
-    console.log("Something has gone wrong...")
+     result = "Something has gone wrong...";
 }
+return result;
+}
+
+
+// function to play five games and keep tally
+function fiveRounds() {    
+    for (let times = 0; times < 5; times++) {
+        console.log(playRound(getComputerChoice(), playerSelection));
+        }
+    }
+
+fiveRounds();
